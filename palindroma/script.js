0c270d7dffd613b2e0f2bea80fb9194a5baba6c0
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-04 14:36:00 +0200

    Palindroma: comment added

--- palindroma/script.js
+++ palindroma/script.js
@@ -3,6 +3,6 @@ function checkPalindrome() {
     const word = document.getElementById('wordInput').value; // Recuperiamo input utente
     const reversedWord = word.split('').reverse().join(''); // Dividiamo, invertiamo e uniamo la parola inserita
     // Restituiamo il risultato
-    const result = word === reversedWord ? 'La parola è palindroma' : 'La parola non è palindroma';
+    const result = word === reversedWord ? 'La parola è palindroma' : 'La parola non è palindroma'; // ? Operatore ternario applicato che funge da condizione if-else; trovato nella documentazione :)))
     document.getElementById('palindromeResult').innerText = result;
 }
