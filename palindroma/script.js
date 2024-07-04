5d885e4ab890f256163fff49841bf87174dad993
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-04 14:29:26 +0200

    Palindroma: get user input

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,4 +1,4 @@
 // Funzione per verificare se una parola è palindroma
 function checkPalindrome() {
-    
+    const word = document.getElementById('wordInput').value; // Recuperiamo input utente
 }
