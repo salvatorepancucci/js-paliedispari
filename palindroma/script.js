fac898dd02d9d4837834d2fcef86271c6b029e76
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-04 14:31:04 +0200

    Palindroma: split-reverse-join to user input to reverse the word entered

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,4 +1,5 @@
 // Funzione per verificare se una parola è palindroma
 function checkPalindrome() {
     const word = document.getElementById('wordInput').value; // Recuperiamo input utente
+    const reversedWord = word.split('').reverse().join(''); // Dividiamo, invertiamo e uniamo la parola inserita
 }
