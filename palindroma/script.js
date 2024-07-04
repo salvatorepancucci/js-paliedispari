62c427e08ea78f8c5af10ea39d89798eb3ee70da
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-04 14:32:58 +0200

    Palindroma: get result to user on index.html

--- palindroma/script.js
+++ palindroma/script.js
@@ -2,4 +2,7 @@
 function checkPalindrome() {
     const word = document.getElementById('wordInput').value; // Recuperiamo input utente
     const reversedWord = word.split('').reverse().join(''); // Dividiamo, invertiamo e uniamo la parola inserita
+    // Restituiamo il risultato
+    const result = word === reversedWord ? 'La parola è palindroma' : 'La parola non è palindroma';
+    document.getElementById('palindromeResult').innerText = result;
 }
