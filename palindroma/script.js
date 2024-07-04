47bca9093dccd08d5d039eeb9b0041495b506b03
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-04 16:33:25 +0200

    Palindroma: 'for loop' limited edition dropped as requested

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,7 +1,21 @@
+// // Funzione per verificare se una parola è palindroma
+// function checkPalindrome() {
+//     const word = document.getElementById('wordInput').value; // Recuperiamo input utente
+//     const reversedWord = word.split('').reverse().join(''); // Dividiamo, invertiamo e uniamo la parola inserita
+//     // Restituiamo il risultato
+//     const result = word === reversedWord ? 'La parola è palindroma' : 'La parola non è palindroma'; // ? Operatore ternario applicato che funge da condizione if-else; trovato nella documentazione :)))
+//     document.getElementById('palindromeResult').innerText = result;
+// }
+
+// For loop Limited Edition
 // Funzione per verificare se una parola è palindroma
 function checkPalindrome() {
     const word = document.getElementById('wordInput').value; // Recuperiamo input utente
-    const reversedWord = word.split('').reverse().join(''); // Dividiamo, invertiamo e uniamo la parola inserita
+    let reversedWord = '';
+    // Usiamo un ciclo for per invertire la parola
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversedWord += word[i];
+    } //Thanks to Fabri's livecoding ;P
     // Restituiamo il risultato
     const result = word === reversedWord ? 'La parola è palindroma' : 'La parola non è palindroma'; // ? Operatore ternario applicato che funge da condizione if-else; trovato nella documentazione :)))
     document.getElementById('palindromeResult').innerText = result;
